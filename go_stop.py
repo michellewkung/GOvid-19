c3f90d7ae4672d0bd35852fdf3a20e627f0b96d2
michellewkung <michellewkung@gmail.com>
2020-05-10 15:16:42 -0400

    Update go_stop.py

--- go_stop.py
+++ go_stop.py
@@ -1,24 +1,25 @@
 from microbit import *
-import time
+import utime 
+import machine
 
-
+#let PIR motion sensor calibrate
+sleep(10000)
 people = 0
-t_end = time.time() + 60 * 15
+button = button_a or button_b
 
-while time.time() < t_end: 
+
+while True: 
+    #display.show(running_time()/1000)
+    if running_time()<60000 * 15:
 # read PIR motion sensor    
-    reading = pin0.read_digital()
-    sleep (10000)
-    if reading == 1:
-        people += 1;
-    if button_a or button_b was_pressed and people =< 15:
-        display.scroll("GO")
-    elif button_a or button_b was_pressed and people => 15:
-        display.scroll("STOP!")
-
+        reading = pin0.read_digital();
+        sleep (10);
+        if reading == 1:
+            people += 1;
+        if button.was_pressed() and people <= 15:
+            display.scroll("GO");
+        elif button.was_pressed() and people >= 15:
+            display.scroll("STOP!");
+    else:
+        machine.reset();
         
-        
-
-
-    
-    
