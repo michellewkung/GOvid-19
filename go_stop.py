d7014f9e2af5fe623b95a79c75b936736ef579e3
michellewkung <michellewkung@gmail.com>
2020-05-10 16:33:52 -0400

    Update go_stop.py

--- go_stop.py
+++ go_stop.py
@@ -5,21 +5,23 @@ import machine
 #let PIR motion sensor calibrate
 sleep(10000)
 people = 0
-button = button_a or button_b
+
 
 
 while True: 
     #display.show(running_time()/1000)
-    if running_time()<60000 * 15:
-# read PIR motion sensor    
-        reading = pin0.read_digital();
-        sleep (10);
+    if running_time() < 60000 * 15:
+        reading = pin0.read_digital()
+        sleep (10000);
         if reading == 1:
             people += 1;
-        if button.was_pressed() and people <= 15:
-            display.scroll("GO");
-        elif button.was_pressed() and people >= 15:
-            display.scroll("STOP!");
+        if button_a.was_pressed() and people <= 15:
+                display.scroll("GO");
+        elif button_a.was_pressed() and people >= 15:
+                display.scroll("STOP!");
+        elif button_b.was_pressed() and people <= 15:
+                display.scroll("GO");
+        elif button_b.was_pressed() and people >= 15:
+                display.scroll("STOP!");
     else:
         machine.reset();
-        
