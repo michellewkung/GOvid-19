60b47e30bf9aa261df4ea7f760bcd92e3b97bb69
michellewkung <michellewkung@gmail.com>
2020-05-11 14:44:09 -0400

    Update govid-19.py

--- govid-19.py
+++ govid-19.py
@@ -2,26 +2,23 @@ from microbit import *
 import utime 
 import machine
 
-#let PIR motion sensor calibrate
+#let PIR motion sensor sensitize
 sleep(10000)
 people = 0
 
-
-
+#Main loop
 while True: 
     #display.show(running_time()/1000)
     if running_time() < 60000 * 15:
-        reading = pin0.read_digital()
-        sleep (10000);
-        if reading == 1:
+        if pin0.read_digital():
             people += 1;
-        if button_a.was_pressed() and people <= 15:
-                display.scroll("GO");
-        elif button_a.was_pressed() and people >= 15:
-                display.scroll("STOP!");
-        elif button_b.was_pressed() and people <= 15:
-                display.scroll("GO");
-        elif button_b.was_pressed() and people >= 15:
-                display.scroll("STOP!");
+    if button_a.was_pressed() and people <= 15:
+        display.scroll("GO");
+    elif button_a.was_pressed() and people >= 15:
+        display.scroll("STOP!");
+    elif button_b.was_pressed() and people <= 15:
+        display.scroll("GO");
+    elif button_b.was_pressed() and people >= 15:
+         display.scroll("STOP!");
     else:
         machine.reset();
